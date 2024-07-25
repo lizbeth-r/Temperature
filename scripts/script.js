c7e788ba636c4f8474b0480eb6e0c4057e28f3a6
Kale e <kalee@Kales-MacBook-Pro.local>
2024-07-25 12:10:03 -0700

    Simple Calculator

--- scripts/script.js
+++ scripts/script.js
@@ -5,23 +5,27 @@ var operation = prompt("Menu: 1.Sum | 2.Sub | 3.Div | 4.Mult");
 if(operation=="1"){
     var sum=Number(num1)+Number(num2);
     document.write(num1 + "+" + num2 + "=" + sum);
-    console.log("Sum:"+sum);
+    console.log("Addition");
+    console.log("Result:"+sum);
 }
 
 if(operation=="2"){
     var sub=Number(num1)-Number(num2);
     document.write(num1 + "-" + num2 + "=" + sub);
-    console.log("Subtraction:"+sub);
+    console.log("Subtraction");
+    console.log("Result:"+sub);
 }
 
 if(operation=="3"){
     var div=Number(num1)/Number(num2);
     document.write(num1 + "/" + num2 + "=" + div);
-    console.log("Division:"+div);
+    console.log("Division");
+    console.log("Result:"+div);
 }
 
 if(operation=="4"){
     var mult=Number(num1)*Number(num2);
     document.write(num1 + "*" + num2 + "=" + mult);
-    console.log("Multiplication:"+mult);
+    console.log("Multiplication");
+    console.log("Result:"+mult);
 }
