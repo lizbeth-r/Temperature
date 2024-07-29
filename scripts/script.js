656d1b9f3ef18800532e00c27b9af2ee649c78e0
Kale e <kalee@Kales-MacBook-Pro.local>
2024-07-29 15:31:26 -0700

    Temperature Assignment

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,13 @@
-var num1 = prompt("Enter your number");
-var num2 = prompt("Enter yout number 2");
-var operation = prompt("Menu: 1.Sum | 2.Sub | 3.Div | 4.Mult");
+function temp() {
+    var temperature = Number(prompt("Enter temperature in Celsius:"));
+    var fahrenheit = (temperature * 9/5) + 32;
 
-if(operation=="1"){
-    var sum=Number(num1)+Number(num2);
-    document.write(num1 + "+" + num2 + "=" + sum);
-    console.log("Addition");
-    console.log("Result:"+sum);
-}
+    console.log(temperature);
+    console.log(fahrenheit);
 
-if(operation=="2"){
-    var sub=Number(num1)-Number(num2);
-    document.write(num1 + "-" + num2 + "=" + sub);
-    console.log("Subtraction");
-    console.log("Result:"+sub);
-}
-
-if(operation=="3"){
-    var div=Number(num1)/Number(num2);
-    document.write(num1 + "/" + num2 + "=" + div);
-    console.log("Division");
-    console.log("Result:"+div);
-}
-
-if(operation=="4"){
-    var mult=Number(num1)*Number(num2);
-    document.write(num1 + "*" + num2 + "=" + mult);
-    console.log("Multiplication");
-    console.log("Result:"+mult);
+    document.getElementById("result").innerHTML += `
+        <div class="text-result">
+            <p>The conversion of Celsius ${temperature}º to Fahrenheit is ${fahrenheit}º</p>
+        </div>
+    `;
 }
