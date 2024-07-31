f4429389ec36610c4222e6a31e8d09ac332aa58d
Kale e <kalee@Kales-MacBook-Pro.local>
2024-07-30 21:59:14 -0700

    Assigment 3

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,21 @@
-function temp() {
-    var temperature = Number(prompt("Enter temperature in Celsius:"));
-    var fahrenheit = (temperature * 9/5) + 32;
+function convertTemperature() {
+    var type = prompt("Enter 'C' for Celsius or 'F' for Fahrenheit:");
+    var temperature = Number(prompt("Enter the temperature:"));
+    
 
-    console.log(temperature);
-    console.log(fahrenheit);
-
-    document.getElementById("result").innerHTML += `
-        <div class="text-result">
-            <p>The conversion of Celsius ${temperature}º to Fahrenheit is ${fahrenheit}º</p>
-        </div>
-    `;
+    if (type == 'C' || type == 'c') {
+        var fahrenheit = (temperature * 9 / 5) + 32;
+        document.getElementById("result").innerHTML += `
+            <div class="text-result">
+                <p>The conversion of Celsius ${temperature}º to Fahrenheit is ${fahrenheit}º</p>
+            </div>
+        `;
+    } else {
+        var celsius = (temperature - 32) * 5 / 9
+        document.getElementById("result").innerHTML += `
+            <div class="text-result">
+                <p>The conversion of Fahrenheit ${temperature}º to  is Celsius ${celsius}º</p>
+            </div>
+        `;
+    }
 }
